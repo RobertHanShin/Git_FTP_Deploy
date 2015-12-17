c02b7ff305df2b9853c1145c43ed1eb9506eb4c4
한신 <Robert@yellotravel.com>
2015-12-17 15:47:42 +0900

    git ftp Init git ftp push 스크립트

---
+++ gitFtpInit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# eppz!tools
+
+# SourceTree Custom Action parameter hooks goes like
+# $PATH <ftp username> <ftp password> <ftp path>
+
+REPOSITORY_PATH="$1"
+FTP_USERNAME="$2"
+FTP_PASSWORD="$3"
+FTP_PATH="$4"
+
+echo "Switching directory to repository root at '$REPOSITORY_PATH'"
+cd "$REPOSITORY_PATH"
+
+echo "Pushing latest checkout to '$FTP_PATH' with the given credentials"
+/usr/local/bin/git-ftp init -u $FTP_USERNAME -p $FTP_PASSWORD "$FTP_PATH"
+
+exit 0
