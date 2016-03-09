3e353f101d38ce463915d99518425fbca5a8af9c
한신 <Robert@yellotravel.com>
2016-03-09 13:55:33 +0900

    CustomAction 잘 되도록 수정.

--- gitFtpPush.sh
+++ gitFtpPush.sh
@@ -2,18 +2,11 @@
 
 # eppz!tools
 
-# SourceTree Custom Action parameter hooks goes like
-# $PATH <ftp username> <ftp password> <ftp path>
-
 REPOSITORY_PATH="$1"
-FTP_USERNAME="$2"
-FTP_PASSWORD="$3"
-FTP_PATH="$4"
+echo "'$REPOSITORY_PATH'"
 
-echo "Switching directory to repository root at '$REPOSITORY_PATH'"
 cd "$REPOSITORY_PATH"
 
-echo "Pushing latest checkout to '$FTP_PATH' with the given credentials"
-/usr/local/bin/git-ftp push -u $FTP_USERNAME -p $FTP_PASSWORD -A "$FTP_PATH"
+/usr/local/bin/git-ftp push -A
 
 exit 0
